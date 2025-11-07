97ab407c443440831e89d0817395f32d8cae2d72
iandolorito <iandolorito16@gmail.com>
2025-11-07 13:50:36 +0800

    yipii

--- testAuth.js
+++ testAuth.js
@@ -1,4 +1,3 @@
 const { registerUser, loginUser } = require('./auth');
 
-// registerUser("johndoe", "john@example.com", "Password123!", "John", "Doe");
 loginUser("john.doe@example.com", "$2b$10$hashedPasswordHere");
